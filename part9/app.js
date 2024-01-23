6f04e9d92b7daa7c3f6db6c499935dcdb29a5d34
shreyans raj <shreyansraj181@gmail.com>
2024-01-24 00:26:37 +0530

    revising DOM

--- part9/app.js
+++ part9/app.js
@@ -1,8 +1,26 @@
-//console.dir(document);
+// console.dir(document);
+// console.dir(document.all);
+// console.dir(document.all[9]);
+// console.dir(document.all[9].innerText);
+// document.all[9].innerText="Peter Parker";
 
-let smallImages=document.getElementsByClassName("oldImg");
+//  let smallImages=document.getElementsByClassName("oldImg");
 
-for(let i=0; i<smallImages.length; i++){
-    console.dir(smallImages[i].src);
-}
+//     for(let i=0; i<smallImages.length; i++){
+//     console.dir(smallImages[i].src);
+// }
+
+// using query selector : select single element
+// document.querySelector("h1");
+// document.querySelector("#description");
+// document.querySelector(".oldImg");
+
+// document.querySelector(("div a")); // selecting first anchor tag under div
+
+// document.querySelectorAll("p") // select all paragraph
+
+
+let para= document.querySelector("p");
+para.innerText="Hi, I am <b>Peter Parker!<b>";
+para.innerHTML="Hi, I am <b>Peter Parker!<b>";
 
