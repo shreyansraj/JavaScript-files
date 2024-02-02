32b2ee24c787bc9b8c63da18dfdfc3fef23b5a78
shreyans raj <shreyansraj181@gmail.com>
2024-02-02 17:01:04 +0530

    MY TODO

---
+++ javaScript TODO/script.js
@@ -0,0 +1,43 @@
+// let div=document.querySelector("div");
+// let ul=document.querySelector("ul");
+// let li=document.querySelectorAll("li");
+
+// div.addEventListener("click", function(){
+//     console.log("div was clicked");
+// });
+
+// ul.addEventListener("click", function(event){
+//     event.stopPropagation();
+//     console.log("ul was clicked");
+// });
+
+// for(li of li){
+// li.addEventListener("click", function(event){
+//     event.stopPropagation();
+//     console.log("li was clicked");
+// });
+// }
+let btn=document.querySelector("button");
+let inp=document.querySelector("input");
+let ul=document.querySelector("ul");
+
+btn.addEventListener("click", function(){
+    let item=document.createElement("li");
+    item.innerText=inp.value;
+
+    let delBtn=document.createElement("button");
+    delBtn.innerText="delete";
+    delBtn.classList.add("delete");
+
+    item.appendChild(delBtn);
+    ul.appendChild(item);
+    inp.value="";
+});
+
+ul.addEventListener("click", function(event){
+    if(event.target.nodeName=="BUTTON"){
+        let listItem=event.target.parentElement;
+        listItem.remove();
+    }
+});
+
--- part10/index.js
+++ part10/index.js
@@ -5,27 +5,72 @@
 //     console.log("button is clicked");
 // };
 
-let btns = document.querySelectorAll(".btn");
+// let btns = document.querySelectorAll(".btn");
 
-for(btns of btns){
-    // btns.onclick=sayHello;
-    // //btns.onclick=sayName;
-    // console.dir(btns);
+// for(btns of btns){
+//     // btns.onclick=sayHello;
+//     // //btns.onclick=sayName;
+//     // console.dir(btns);
 
-    // btns.addEventListener("click", function(){
-    //     console.log("Hello");
-    //     console.log("Shreyans");
-    // });
+//     // btns.addEventListener("click", function(){
+//     //     console.log("Hello");
+//     //     console.log("Shreyans");
+//     // });
 
-    btns.addEventListener("click", sayHello);
-    btns.addEventListener("click", sayName);
+//     btns.addEventListener("click", sayHello);
+//     btns.addEventListener("click", sayName);
+// }
+
+// function sayHello(){
+//     console.log("Hello");
+// }
+// function sayName(){
+//     console.log("Shreyans");
+// }
+
+let h1=document.querySelector("h1");
+let h2=document.querySelector("h2");
+let p=document.querySelector("p");
+let button=document.querySelector("button");
+
+function changeColor(){
+    this.style.backgroundColor="orange";
 }
 
-function sayHello(){
-    console.log("Hello");
-}
-function sayName(){
-    console.log("Shreyans");
-}
+button.addEventListener("click", changeColor);
+h1.addEventListener("click", changeColor);
+h2.addEventListener("click", changeColor);
+p.addEventListener("click", changeColor);
+
+// --------------keyword event-------------------
+let input=document.querySelector("input");
+
+input.addEventListener("keydown", function(event){
+    console.log("key=", event.key);
+    console.log("code=", event.code);
+    console.log("key was pressed");
+});
+
+// ------------------form event-------------------
+
+let form=document.querySelector("form");
+
+// form.addEventListener("submit", function(event){
+//     event.preventDefault();
+//     alert("form submitted");
+// });
+
+// ------Extracting Form Data---------------
+form.addEventListener("submit", function(event){
+    event.preventDefault();
+    
+    let inp=document.querySelector("#forminput");
+    console.dir(inp);
+    console.log(inp.value);
+});
+
+
+
+
 
 
